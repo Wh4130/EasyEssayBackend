de0b684719283210bc0d4f8fcf06e1200919e5e7
Huang Lin Chun <huang0jin@gmail.com>
2026-01-15 17:25:16 +0800

    Updated config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    REDIS_URL: str = "redis://localhost:6379/0"
     model_config = SettingsConfigDict(env_file=".env")
 
 Config = Settings()
 
-broker_url = Config.model_config.REDIS_URL
-result_backend = Config.model_config.REDIS_URL
+broker_url = Config.REDIS_URL
+result_backend = Config.REDIS_URL
 
