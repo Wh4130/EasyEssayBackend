b42e2a8d7845fab4df4a2e7323d99ce6f852afe1
Huang Lin Chun <huang0jin@gmail.com>
2026-01-15 17:41:39 +0800

    Updated config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    REDIS_URL: str = "redis://localhost:6379/0"
-    model_config = SettingsConfigDict(env_file=".env")
+    REDIS_URL: str
+    model_config = SettingsConfigDict(env_file=".env", extra = "ignore")
 
 Config = Settings()
 
